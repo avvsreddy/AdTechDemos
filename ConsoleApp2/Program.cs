13c7b758e025db68f3006c243fd7a046f25dd6ee
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-01-23 13:28:30 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            M();
+            M();
+            M();
+
+        }
+
+        static void M()
+        {
+            int a;
+            a = 10;
+            string str;
+            str = "Hello";
+        }
+    }
+}
