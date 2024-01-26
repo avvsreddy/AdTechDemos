3609c378655825c7ea030aab3287c884ab881afa
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-01-26 11:09:56 +0530

    training-trainer code

--- TrainerTraineeApp/Program.cs
+++ TrainerTraineeApp/Program.cs
@@ -13,7 +13,44 @@
             trainer.TheOrganization = org;
 
             Console.WriteLine($"The Training Org Name : {training.GetTrainingOrgName()}");
+
+            // create trainees and add to training
+            Trainee t1 = new Trainee();
+            Trainee t2 = new Trainee();
+            Trainee t3 = new Trainee();
+
+            training.Trainees.Add(t1);
+            training.Trainees.Add(t2);
+            training.Trainees.Add(t3);
+
+
             Console.WriteLine($"No. of trainees: {training.GetNumberOfTrainees()}");
+
+            Course course = new Course();
+            training.Course = course;
+            Module m1 = new Module();
+            Module m2 = new Module();
+            Module m3 = new Module();
+            course.Modules.Add(m1);
+            course.Modules.Add(m2);
+            course.Modules.Add(m3);
+
+            Unit u1 = new Unit();
+            u1.Duration = 5;
+            Unit u2 = new Unit { Duration = 10 };
+            m1.Units.Add(u1);
+            m1.Units.Add(u2);
+
+            Unit u3 = new Unit { Duration = 10 };
+            Unit u4 = new Unit { Duration = 10 };
+            m2.Units.Add(u3);
+            m2.Units.Add(u4);
+            Unit u5 = new Unit { Duration = 10 };
+            m3.Units.Add(u5);
+
+
+
+
             Console.WriteLine($"Training Duration in Hours: {training.GetTrainingDurationInHrs()}");
         }
     }
@@ -39,7 +76,7 @@
     {
         public Trainer TheTrainer { get; set; }
         public List<Trainee> Trainees { get; set; } = new List<Trainee>();
-
+        public Course Course { get; set; }
         public string GetTrainingOrgName()
         {
             // return training org name
@@ -49,13 +86,44 @@
         public int GetNumberOfTrainees()
         {
             // count the no. of participants and return
-            return 0;
+
+            return Trainees.Count;
         }
 
         public int GetTrainingDurationInHrs()
         {
             // calculate the duration and return
-            return 0;
+            int totDuration = 0;
+            // for each modules
+            foreach (var module in Course.Modules)
+            {
+                // for each unit
+                foreach (var unit in module.Units)
+                {
+                    totDuration += unit.Duration;
+                }
+            }
+
+            return totDuration;
         }
+    }
+
+    class Course
+    {
+        public List<Training> Trainings { get; set; } = new List<Training>();
+        public List<Module> Modules { get; set; } = new List<Module>();
+    }
+    class Module
+    {
+        public List<Unit> Units { get; set; } = new List<Unit>();
+    }
+    class Unit
+    {
+        public int Duration { get; set; }
+        public List<Topic> Topics { get; set; } = new List<Topic>();
+    }
+    class Topic
+    {
+        public string Name { get; set; }
     }
 }
