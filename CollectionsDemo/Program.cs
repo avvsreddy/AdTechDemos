4a2903a69cb9a7e19634a9fde2c3aa8ab7ec6040
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-01-27 13:31:15 +0530

    OO labs

---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace CollectionsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int data = 10; // scalar - value type
+
+            int[] datas = new int[10]; // ref type
+            datas[0] = 10;
+
+            datas[1] = 10;
+            int a = datas[0];
+
+            int[] numbers1 = new int[10];
+            int[] numbers2 = new int[3] { 1, 2, 3 };
+            int[] numbers3 = new int[] { 1, 2, 3 };
+            int[] numbers4 = { 1, 2, 3, 4 };
+
+            int[] numbers = { 1, 345, 2, 78, 234, 78, 454, 4, 679, 45, 34 };
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[100]);
+            //}
+
+            // find sum
+            int sum = numbers.Sum();
+            int min = numbers.Min();
+            int max = numbers.Max();
+            double avg = numbers.Average();
+
+            // sort
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 2 dim
+
+            int[,] twod = new int[5, 3];
+
+            // Jagged Arrays
+            // step 1: create rows
+            int[][] j = new int[3][];
+            // next stepts : create cols for each row
+            j[0] = new int[3];
+
+
+        }
+    }
+}
---
+++ DynamicCollectionDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace DynamicCollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // need to process n number of int
+            // Typed Dynamic Collection
+
+            List<int> ints = new List<int>();
+            // add number
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(2);
+
+            // read
+
+            int x = ints[0];
+
+            // read all
+
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+
+            Stack<int> stack = new Stack<int>();
+            // add
+            stack.Push(10);
+            stack.Push(20);
+
+            // reads = lifo
+            int a = stack.Pop(); // reads and deletes
+            a = stack.Peek(); // only reads
+
+            Queue<int> q = new Queue<int>();
+            //add
+            q.Enqueue(10);
+            q.Enqueue(20);
+
+            // read fifo
+            a = q.Dequeue(); //reads and removes
+            // read only
+            a = q.Peek(); // only reads
+
+
+            Dictionary<int, string> results = new Dictionary<int, string>();
+            // add
+            results.Add(111, "Pass");
+            results.Add(222, "Fail");
+
+            // read with key
+
+            var r = results[111];
+
+            HashSet<int> set = new HashSet<int>();
+            // add
+            set.Add(1);
+            set.Add(2);
+            set.Add(2);
+
+        }
+    }
+}
