47ce73009d9b84f99173c8e53966870bedae71a1
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-01-29 11:26:00 +0530

    ex1

---
+++ ExceptionsDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExceptionsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception Handling Demo");
+
+            // accept two ints and find the sum continuesly
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter First Number: ");
+                    int fno = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Second Nummber: ");
+                    int sno = int.Parse(Console.ReadLine());
+                    // find the sum
+                    int sum = fno + sno;
+                    // display the result
+                    Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+                    Console.WriteLine("--------------------------------------");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter only integer number");
+                }
+            }
+
+
+        }
+    }
+}
