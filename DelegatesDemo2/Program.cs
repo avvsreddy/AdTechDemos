eed84ccf804b0e25b282567c51804b88b40bb3b3
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-01-30 13:11:44 +0530

    lambda

--- DelegatesDemo2/Program.cs
+++ DelegatesDemo2/Program.cs
@@ -8,27 +8,57 @@ namespace DelegatesDemo2
         {
             ProcessManager pMgr = new ProcessManager();
             //client developer 1
-            //pMgr.ShowProcessList();
+            //pMgr.ShowProcessList(NoFilter);
+            pMgr.ShowProcessList(p => true);
             // client 2
             //FilterDelegate filter = new FilterDelegate(FilterByName);
             //pMgr.ShowProcessList(filter);
+
+            // Anonymous Delegates - holds anoymous method address
+
+            pMgr.ShowProcessList(delegate (Process p)
+                {
+                    return p.ProcessName.StartsWith("S");
+                }
+            );
+
+            // Lambda - Expressions - Light Weight Syntax for Anonymous Delegates
+
+            pMgr.ShowProcessList((Process p) =>
+            {
+                return p.ProcessName.StartsWith("S");
+            }
+           );
+
+            // Lambda - Statements - Expressions
+
+            pMgr.ShowProcessList(p => p.ProcessName.StartsWith("S"));
+
+
             // client 3
-            pMgr.ShowProcessList(FilterByMemSize);
+            //pMgr.ShowProcessList(FilterByMemSize);
+            pMgr.ShowProcessList(p => p.WorkingSet64 >= 234234234234);
         }
-        // client 2
-        public static bool FilterByName(Process p)
+        // client 1
+        public static bool NoFilter(Process p)
         {
-            return p.ProcessName.StartsWith("S");
+            return true;
         }
+
+
+        // client 2
+        //public static bool FilterByName(Process p)
+        //{
+        //    return p.ProcessName.StartsWith("S");
+        //}
 
 
 
         // client 3
         public static bool FilterByMemSize(Process p)
         {
-            if (p.WorkingSet64 >= 100 * 1024 * 1024)
-                return true;
-            else return false;
+            return (p.WorkingSet64 >= 100 * 1024 * 1024);
+
         }
 
     }
