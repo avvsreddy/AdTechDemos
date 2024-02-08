fd1c71557fdd4caddb42f8f7f18ea28484843fe0
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-02-08 13:27:42 +0530

    ef demo code

---
+++ ProductECatelog.DataAccess/ProductECatelogDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProductECatelog.Entities;
+
+namespace ProductECatelog.DataAccess
+{
+    public class ProductECatelogDataContext : DbContext
+    {
+        // Configure/Map Database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ProductsECatelogDB;Integrated Security=true");
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        // Configure/Map: Entity Class => Table
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ ProductECatelog.UI.ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using ProductECatelog.DataAccess;
+using ProductECatelog.Entities;
+
+namespace ProductECatelog.UI.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void Edit()
+        {
+            ProductECatelogDataContext db = new ProductECatelogDataContext();
+
+            // edit a product price
+            // Step 1: get the product to edit
+            var productToEdit = db.Products.Find(2);
+            // step 2: change the property values except pk
+            productToEdit.Price = 150000;
+            // Step 3: update database
+            db.SaveChanges();
+        }
+
+        private static void Delete()
+        {
+            // delete some product
+            // Step 1: get the product to delete from dbset
+            ProductECatelogDataContext db = new ProductECatelogDataContext();
+            var productToDel = from p in db.Products
+                               where p.ProductID == 1
+                               select p;
+            // Step 2: remove that product from dbset
+            db.Products.Remove(productToDel.FirstOrDefault());
+            // Step 3: save the changes of dbset into database
+            db.SaveChanges();
+        }
+
+        private static void GetAll()
+        {
+            // Get all Products and display
+            ProductECatelogDataContext db = new ProductECatelogDataContext();
+            // LINQ to Entites
+            var allProducts = from p in db.Products
+                              where p.Price <= 100000
+                              select p;
+            foreach (var p in allProducts)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+
+        private static void Insert()
+        {
+            // save product info
+            Product p = new Product { Name = "IPhone 15", Price = 100000 };
+            ProductECatelogDataContext db = new ProductECatelogDataContext();
+            db.Products.Add(p);
+            db.SaveChanges();
+            Console.WriteLine("Product Saved...");
+        }
+    }
+}
